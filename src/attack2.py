42126bb5f35a4d9adef3c41de1c73846e3d334ab
Anton Melnikov <melnikov.anton@gmail.com>
2014-06-03 23:32:03 +0400

    SYNC

---
+++ src/attack2.py
@@ -0,0 +1,35 @@
+# Single bullet attacks
+def abstract(projectile_type, starting_pos, speed, aimed_at=None):
+    pass
+
+
+def linear(projectile_type, starting_pos, speed, aimed_at=None, angle=0):
+    pass
+
+
+def arc(projectile_type, starting_pos, speed):
+    pass
+
+
+def wave(projectile_type, starting_pos, speed):
+    pass
+
+
+def circular(projectile_type, starting_pos, speed):
+    pass
+
+
+
+
+
+# Multiple bullet attacks
+def shotgun(projectile_type, starting_pos, speed, aimed_at=None):
+    pass
+
+
+def flak(projectile_type, starting_pos, speed, aimed_at=None):
+    pass
+
+
+def radial(projectile_type, starting_pos, speed, aimed_at=None):
+    pass
